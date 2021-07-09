aeb42e66d0c9769122e7190924224e59eb90db6a
JasonHJ <hejian_best@163.com>
2021-07-09 16:05:36 +0800

    add metamask support

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import { LockPlugin } from '@snapshot-labs/lock/plugins/vue';
 import injected from '@snapshot-labs/lock/connectors/injected';
+import injectedMetaMask from '@snapshot-labs/lock/connectors/injectedMetaMask';
 import fortmatic from '@snapshot-labs/lock/connectors/fortmatic';
 import portis from '@snapshot-labs/lock/connectors/portis';
 import walletconnect from '@snapshot-labs/lock/connectors/walletconnect';
@@ -8,7 +9,7 @@ import walletlink from '@snapshot-labs/lock/connectors/walletlink';
 import config from '@/helpers/config';
 
 const options: any = { connectors: [] };
-const connectors = { injected, fortmatic, portis, walletconnect, walletlink };
+const connectors = { injected, injectedMetaMask, fortmatic, portis, walletconnect, walletlink };
 Object.entries(config.connectors).forEach((connector: any) => {
   options.connectors.push({
     key: connector[0],
